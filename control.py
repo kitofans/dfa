9349765427a0348829a3dfc9f828f29896e22be8
Ankit Kumar <ankitk@stanford.edu>
2015-01-21 22:51:33 -0800

    pushing for tmux stuff

--- control.py
+++ control.py
@@ -9,7 +9,7 @@ import pickle
 if theano.config.floatX == 'float32':
 	ts = generate_train_set_gpu(10000,10)
 else:
-	ts = generate_train_set(100,10)
+	ts = generate_train_set(10000,10)
 dfa= theanoDFA(15,len(total),init=2)
 for epoch in xrange(1000):
 	tic = time.time()
@@ -18,7 +18,7 @@ for epoch in xrange(1000):
 		# O2,F2,transition_tensor2,cost2 = dfa_2.trainmb(X,y)
 	toc = time.time()
 	print "Epoch %s took %s seconds." % (str(epoch), str(toc - tic))
-	if epoch+1 % 200 == 0:
+	if epoch+1 % 100 == 0:
 		pickle.dump(dfa, open('epoch_%s.pkl' % epoch,'w'))
 
 pickle.dump(dfa, open('epoch_final.pkl','w'))
